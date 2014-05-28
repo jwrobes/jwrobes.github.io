61837ffbba1598a08859f63c4c4f433672619e20
Jonathan Wrobel <jwrobes@gmail.com>
2014-05-28 11:43:30 -0700

    Fixed conditional structure to cycle through each post and format when appropriate 	*Changed structure to fix variable scope issues

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -54,18 +54,15 @@ BlogView.prototype = {
 			return;
 		}
 		else {
-			debugger
 			window.__post_grid_width = this.setwinwidth = 
 				this.currentwinWidth > 780 ? 10000 :
 				this.currentwinWidth > 680 ? 780 :
 																			680;
-		debugger
 		}
 	},
 
 	fetchRecentPosts: function () {
 		this.posts = document.getElementById('recent-posts');
-		debugger
 		return this.posts
 	},
 
@@ -86,42 +83,49 @@ BlogView.prototype = {
 		positionPosts: function(child_nodes) {
 			var child, node, col_width, col0_y_position, col1_x_position, col1_y_position, origin = {x:0, y:0}, node_count = 0, max_y_of_first_row
 			for (child in child_nodes) {
-				debugger
 				node = child_nodes[child];
 				if (checkIfNode(node)) {
 					if (checkifBreaker(node)) {
 						if (oneColumnOnly) {
 							node.style.height = null;
-						}	
-						else {
-							setBreakerDivHeightMatch(node)
+						}	else {
+								node.style.height = (Math.max(col1_y_position, col0_y_position) - max_y_of_first_row) + 'px'
 						}
 						break
-					}
-					if (firstPostColumn0) {
-						setPositionFirstPost(node)
-					}
-					if (firstPostColumn1) {
-						setPositionSecondPost(node)
-					}
-					if (oneColumnOnly) {
-						formatPostOneColumn(node)
-					}
-					else {
-						node.style.position = 'absolute'
-						if (firstColumn(node_count)) {
-							node.style.left = origin.x + 'px'
-							node.style.top = col0_y_position + 'px'
-							col0_y_position += node.clientHeight
-						}
-						else {
-							node.style.left = col1_x_position + 'px'
-							node.style.top = col1_y_position + 'px'
-							col1_y_position += node.clientHeight;
-						}
-					}
-				}
+					}			
+					if (firstPostColumn0(col0_y_position)) {
+						origin.x = node.offsetLeft;
+						origin.y = node.offsetTop;
+						col_width = node.clientWidth;
+						col0_y_position = origin.y + node.clientHeight;
+						max_y_of_first_row = col0_y_position;
+						} else {
+						if (firstPostColumn1(col1_y_position)) {
+							col1_y_position = origin.y + node.clientHeight
+							col1_x_position = origin.x + node.clientWidth;
+							if (col1_y_position > max_y_of_first_row) {
+								max_y_of_first_row = col1_y_position;
+							}
+						} else {
+							if (oneColumnOnly()) {
+								formatPostOneColumn(node)
+							} else {
+								node.style.position = 'absolute'
+								node.style.width = col_width + 'px'
+								if (firstColumn(node_count)) {
+									node.style.left = origin.x + 'px'
+									node.style.top = col0_y_position + 'px'
+									col0_y_position += node.clientHeight
+								}	else {
+									node.style.left = col1_x_position + 'px'
+									node.style.top = col1_y_position + 'px'
+									col1_y_position += node.clientHeight;
+								}
+							}
+						}	
+					}	
 				++node_count;
+				}
 			}
 
 		}
@@ -138,44 +142,44 @@ var checkifBreaker = function(node) {
 }
 
 var oneColumnOnly = function () {
-	window.__post_grid_width <= 680
+	return window.__post_grid_width <= 680
 }
 
 var setNodeHeightNull = function (node) {
 	node.style.height = null;
 } 
 
-var setBreakerDivHeightMatch = function(node) {
-	node.style.height = (Math.max(col1_y_position, col0_y_position) - max_y_of_first_row) + 'px'
-}
+// var setBreakerDivHeightMatch = function(node) {
+// 	node.style.height = (Math.max(col1_y_position, col0_y_position) - max_y_of_first_row) + 'px'
+// }
 
 var firstPostColumn0 = function (col0_y_position) {
-	col0_y_position === undefined
+	return col0_y_position === undefined
 }
 
-var setPositionFirstPost = function(node) {
-	debugger
-	origin.x = node.offsetLeft;
-	origin.y = node.offsetTop;
-	col_width = node.clientWidth;
-	col0_y_position = origin.y + node.clientHeight;
-	max_y_of_first_row = col0_y_position;
-	debugger
-}
+// var setPositionFirstPost = function(node) {
+// 	debugger
+// 	origin.x = node.offsetLeft;
+// 	origin.y = node.offsetTop;
+// 	col_width = node.clientWidth;
+// 	col0_y_position = origin.y + node.clientHeight;
+// 	max_y_of_first_row = col0_y_position;
+// 	debugger
+// }
 
-var firstPostColumn1 = function (col0_y_position) {
+var firstPostColumn1 = function (col1_y_position) {
 	return col1_y_position === undefined
 }
 
 
-var setPositionSecondPost = function(node) {
-	col1_y_position = origin.y + node.clientHeight
-	col1_x_position = origin.x + node.clientWidth;
-	if (col1_y_position > max_y_of_first_row) {
-		max_y_of_first_row = col1_y_position;
-	}
-	debugger
-}
+// var setPositionSecondPost = function(node) {
+// 	col1_y_position = origin.y + node.clientHeight
+// 	col1_x_position = origin.x + node.clientWidth;
+// 	if (col1_y_position > max_y_of_first_row) {
+// 		max_y_of_first_row = col1_y_position;
+// 	}
+// 	debugger
+// }
 
 var formatPostOneColumn = function(node) {
 	  node.style.position = null;
